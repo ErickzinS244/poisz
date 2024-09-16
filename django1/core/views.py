ad2343bd23a91bf49ce3cfb6b9e0be3a72af4c2b
ErickzinS244 <166455743+ErickzinS244@users.noreply.github.com>
2024-09-16 13:42:18 -0300

    Add files via upload

---
+++ django1/core/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+# Create your views here.
+
+def index(request):
+    	    return render(request, 'index.html')
+
+def contato(request):
+            return render(request, 'contato.html')
+def assessment(request):
+    	    return render("assessment")
